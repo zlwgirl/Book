eb5f3dfad0300dc51b77d6c8ab69a6afddf7ce9c
USER-20160906RT\Administrator <1845549666@qq.com>
2016-10-17 16:06:56 +0800

    解决冲突

--- app/src/main/java/com/example/z_lw/aaaa/FreeFragment.java
+++ app/src/main/java/com/example/z_lw/aaaa/FreeFragment.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
